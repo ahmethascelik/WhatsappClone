f62d7341a38d67a08b8fc2487c82192e8900d5f4
ahmethascelik <ahmethascelik@gmail.com>
2023-03-22 15:09:13 +0300

    butonlar falan

--- app/src/main/java/com/teb/whatsappclone/ui/chat/ChatActivity.java
+++ app/src/main/java/com/teb/whatsappclone/ui/chat/ChatActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import com.teb.whatsappclone.R;
+
 public class ChatActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_chat);
+
     }
 }
