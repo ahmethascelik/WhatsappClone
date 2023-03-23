261fa9e1868689ab5ae5159d86f621a25fcd7b4d
ahmethascelik <ahmethascelik@gmail.com>
2023-03-23 16:31:36 +0300

    send message

--- app/src/main/java/com/teb/whatsappclone/ui/chat/ChatActivity.java
+++ app/src/main/java/com/teb/whatsappclone/ui/chat/ChatActivity.java
@@ -2,6 +2,14 @@ package com.teb.whatsappclone.ui.chat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -9,11 +17,68 @@ import com.teb.whatsappclone.R;
 
 public class ChatActivity extends Activity {
 
+    ImageButton backButton;
+    TextView txtUsername;
+    EditText editText;
+
+    ImageButton micButton;
+    ImageButton camButton;
+    ImageButton sendButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_chat);
+
+        initViews();
+
+    }
+
+    private void initViews() {
+        backButton = findViewById(R.id.backbutton);
+        txtUsername = findViewById(R.id.txtUsername);
+
+        camButton = findViewById(R.id.camButton);
+        sendButton = findViewById(R.id.sendButton);
+        micButton = findViewById(R.id.micButton);
+
+        backButton.setOnClickListener(view -> {
+            finish();
+        });
+
+        txtUsername.setText("Ahmet");
+        editText = findViewById(R.id.editText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if(charSequence.length() == 0){
+                    //camera + mic visible | sendbutton gone
+                    camButton.setVisibility(View.VISIBLE);
+                    micButton.setVisibility(View.VISIBLE);
+                    sendButton.setVisibility(View.GONE);
+                }else{
+                    //camera + mic gone | sendbutton visible
+
+                    camButton.setVisibility(View.GONE);
+                    micButton.setVisibility(View.GONE);
+                    sendButton.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
     }
 }
