3065761951d96616665415d53c255c824e046675
ahmethascelik <ahmethascelik@gmail.com>
2023-03-27 14:52:54 +0300

    firebase database eklendi

--- app/src/main/java/com/teb/whatsappclone/data/service/ServiceLocator.java
+++ app/src/main/java/com/teb/whatsappclone/data/service/ServiceLocator.java
@@ -1,10 +1,11 @@
 package com.teb.whatsappclone.data.service;
 
 import com.teb.whatsappclone.data.service.impl.fake.FakeChatService;
+import com.teb.whatsappclone.data.service.impl.firebase.FirebaseChatService;
 
 public class ServiceLocator {
 
     public static ChatService provideChatService(){
-        return new FakeChatService();
+        return new FirebaseChatService();
     }
 }
--- app/src/main/java/com/teb/whatsappclone/data/service/impl/firebase/FirebaseChatService.java
+++ app/src/main/java/com/teb/whatsappclone/data/service/impl/firebase/FirebaseChatService.java
@@ -1,5 +1,7 @@
 package com.teb.whatsappclone.data.service.impl.firebase;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.teb.whatsappclone.data.service.ChatService;
 import com.teb.whatsappclone.data.service.MessageListener;
 
@@ -7,6 +9,11 @@ public class FirebaseChatService implements ChatService {
     @Override
     public void sendMessage(String sender, String message) {
 
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 
     @Override
